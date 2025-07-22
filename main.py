2143522b100e5637b15067263fc49bb201e1967d
Kleiton Da Rosa <73746979+Kleiton559@users.noreply.github.com>
2025-07-22 17:34:43 -0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+#Importação de Bibliotecas
+import os
+
+def display(lista):
+    for item in lista:
+      print(item)
+
+
+
+
+#listar Arquivos dentro de uma pasta
+lista_arquivos = os.listdir("C://Users/estagiario.it/Documents/Cursos/Vendas")
+display(lista_arquivos )
+# for arquivo in lista_arquivos:
+#     print(arquivo)
+##print(lista_arquivos)
