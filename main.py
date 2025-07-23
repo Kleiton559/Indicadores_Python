019f4faedc2fa45f137f474e76e5d63a0dbd51d5
Kleiton Da Rosa <73746979+Kleiton559@users.noreply.github.com>
2025-07-23 14:25:38 -0100

    Add files via upload
    #Atualizacao

--- main.py
+++ main.py
@@ -5,12 +5,19 @@ def display(lista):
     for item in lista:
       print(item)
 
-
-
-
+#Diretório
+diretory = "C:/Users/krena/OneDrive/Documentos/PROJETOS PESSOAIS/DB_vendas/Vendas"
 #listar Arquivos dentro de uma pasta
-lista_arquivos = os.listdir("C://Users/estagiario.it/Documents/Cursos/Vendas")
-display(lista_arquivos )
-# for arquivo in lista_arquivos:
-#     print(arquivo)
-##print(lista_arquivos)
+lista_arquivos = os.listdir(diretory)
+#display(lista_arquivos)
+
+
+#Importar bases de dados de vendas
+for arquivo in lista_arquivos:
+    # Exibir o nome de cada arquivo
+     #print(arquivo)
+
+    #Verificar se é  um arquivo de vendas
+    if "vendas" in arquivo.lower():#arquivo.lower() - tratamento de dados
+     #Importar o Arquivo de Vendas
+     print(f'{diretory}/{arquivo}')
